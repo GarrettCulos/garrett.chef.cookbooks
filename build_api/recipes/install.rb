549c5e0adeb1bc5f8af8e0651adf5f9ceb6cf1c3
garrett <garrettculos@gmail.com>
2018-08-26 20:14:19 -0700

    Update build_api install rb

--- build_api/recipes/install.rb
+++ build_api/recipes/install.rb
@@ -1,6 +1,18 @@
-execute 'git_clone' do 
-    cwd "#{node['project']['path']}"
-    command "git clone #{node['project']['path']} ."
+def directory_exists?(directory)
+    File.directory?(directory)
+end
+if !directory_exists?("#{node['project']['path']}/.git")
+    directory node['project']['path'] do
+        owner 'root'
+        group 'root'
+        mode '0755'
+        action :create
+    end
+
+    execute 'git_clone' do 
+        cwd "#{node['project']['path']}"
+        command "git clone #{node['project']['path']} ."
+    end
 end
 
 execute 'git_pull' do 
